0ee1710f17062f9f271e48041a853b4b89627c16
user <hochu-leto@yandex.ru>
2022-09-05 10:47:30 +0300

    2

--- client.py
+++ client.py
@@ -1,8 +1,20 @@
 import requests
 
+# response = requests.post(
+#     'http://127.0.0.1:6000/users/',
+#     json={'email': "test12@nmmu.ru", 'password': 'tghRYdf45#$'}
+# )
+#
+# response = requests.get(
+#     'http://127.0.0.1:6000/users/'
+# )
+
 response = requests.post(
-    'http://127.0.0.1:6000/users/',
-    json={'email': "test222@nmmu.ru", 'password': 'tghRY45#$'}
+    'http://127.0.0.1:6000/ads/',
+    json={"token": "71a96721-f157-485c-aa33-66590c469442", 'header': "Fourth commit", 'description': 'Lorem ipsum '
+                                                                                                    'caecat '
+                                                                                                    'cupidatat non '
+                                                                                                    'proid'}
 )
 
 print(response.text)
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import atexit
 import json
 import re
 import uuid
+from pprint import pprint
 
 from flask import Flask, request, jsonify
 from flask.views import MethodView
@@ -21,6 +22,8 @@ app = Flask('server')
 atexit.register(lambda: engine.dispose())
 
 PASSWORD_REG = re.compile("^(?=.*[a-z_])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&_])[A-Za-z\d@$!#%*?&_]{8,200}$")
+#UUID_REG = re.compile('^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$')
+UUID_REG = re.compile('^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$')
 
 
 # PASSWORD_REG = re.compile('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$')
@@ -92,12 +95,14 @@ def validation_error_handler(error: ValidationError):
     response.status_code = 400
     return response
 
+
 def check_token(session):
+    r = request.headers
     token = (
         session.query(Token)
         .join(User)
         .filter(
-            User.user_name == request.headers.get("user_name"),
+            User.email == request.headers.get("email"),
             Token.id == request.headers.get("token"),
         )
         .first()
@@ -105,10 +110,32 @@ def check_token(session):
     if token is None:
         raise HttpError(401, "invalid token")
     return token
+
+
 class UserView(MethodView):
 
     def get(self):
-        pass
+        with Session() as ses:
+            # users = ses.query(User)
+            # user_list = []
+            # for user in users:
+            #     user_list.append({'user_id': user.id,
+            #                       'email': user.email,
+            #                       'password': user.password,
+            #                       'token': ses.query(Token)
+            #                      .join(User)
+            #                      .filter(User.id == user.id, )
+            #                      .first()})
+            # pprint(user_list)
+            # return jsonify(user_list)
+            users = ses.query(Token)
+            user_list = []
+            for user in users:
+                user_list.append({'token_id': user.id,
+                                  'creation_time': user.creation_time,
+                                  'user_id': user.user_id})
+            pprint(user_list)
+            return jsonify(user_list)
 
     def post(self):
         json_data = request.json
@@ -116,12 +143,19 @@ class UserView(MethodView):
         with Session() as ses:
             user = User(email=json_data_validated['email'], password=bcrypt.generate_password_hash(
                 json_data_validated['password'].encode()).decode())
+            token = Token(user=user)
             ses.add(user)
+            ses.add(token)
             try:
                 ses.commit()
                 return jsonify({
                     'id': user.id,
-                    'registration_time': user.registration_time.isoformat()
+                    'registration_time': user.registration_time.isoformat(),
+                    'token':
+                        ses.query(Token)
+                        .join(User)
+                        .filter(User.id == user.id, )
+                        .first()
                 })
             except IntegrityError:
                 raise HttpError(400, 'user already exists')
@@ -130,6 +164,7 @@ class UserView(MethodView):
 class AdCreateModel(BaseModel):
     header: str
     description: str
+    token: str
 
     @validator('description')
     def good_description(cls, value):
@@ -149,6 +184,13 @@ class AdCreateModel(BaseModel):
 
         return value
 
+    @validator('token')
+    def valid_token(cls, value):
+        if not UUID_REG.search(value):
+            raise ValueError('token is not valid')
+        return value
+
+
 
 class AvitoView(MethodView):
 
@@ -156,32 +198,43 @@ class AvitoView(MethodView):
         pass
 
     def get(self):
-        pass
+        print('hello')
+        return jsonify({'text': "hello"})
 
-    def post(self, user_id: int):
+    def post(self):
         json_data = request.json
-        json_data_validated = AdCreateModel(header=json_data['header'], description=json_data['description']).dict()
+        json_data_validated = AdCreateModel(header=json_data['header'],
+                                            description=json_data['description'],
+                                            token=json_data['token']).dict()
         with Session() as ses:
-            token = check_token(ses)
-            if token.user.id != user_id:
-                raise HttpError(403, "auth error")
+
+            validate_token = (
+                ses.query(Token)
+                .filter(Token.id == json_data_validated['token'])
+                .first()
+            )
+            if not validate_token:
+                raise HttpError(401, "invalid token")
+
             ad = Ad(
                 header=json_data_validated['header'],
                 description=json_data_validated['description'],
-                user_id=user_id
+                user_id=validate_token.user_id
             )
             ses.add(ad)
             try:
                 ses.commit()
                 return jsonify({
                     'id': ad.id,
-                    'registration_time': ad.registration_time.isoformat()
+                    'registration_time': ad.registration_time.isoformat(),
+                    'description': ad.description
                 })
             except IntegrityError:
-                raise HttpError(400, 'user already exists')
+                raise HttpError(400, 'ad already exists')
 
 
 app.add_url_rule('/users/', view_func=UserView.as_view('create_user'), methods=['POST'])
+app.add_url_rule('/users/', view_func=UserView.as_view('get_users'), methods=['GET'])
 app.add_url_rule('/ads/', view_func=AvitoView.as_view('create_ad'), methods=['POST'])
 app.add_url_rule('/ads/', view_func=AvitoView.as_view('get_ad'), methods=['GET'])
 app.add_url_rule('/ads/', view_func=AvitoView.as_view('delete_ad'), methods=['DELETE'])
